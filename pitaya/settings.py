7be9ff644e83eed75830fc1ae9795cbbfcd52306
Ilana Bonder <ilanabonder@gmail.com>
2016-09-11 12:13:51 +0300

    Upgrading to MySQL

--- pitaya/settings.py
+++ pitaya/settings.py
@@ -97,11 +97,22 @@ WSGI_APPLICATION = 'pitaya.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pitaya',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
